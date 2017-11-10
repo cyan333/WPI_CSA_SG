40e80467c42110e4e8241fec9f14f4709ae1b304
Fangming Ning <synfm@126.com>
2017-11-10 00:17:06 -0500

    Editor icons and toggle code

--- WPI_CSA/Helpers/EditorButton.swift
+++ WPI_CSA/Helpers/EditorButton.swift
@@ -12,21 +12,35 @@ protocol EditorButtonClickDelegate: class {
     func buttonClickedOnIndex(index: Int)
 }
 
+
 class EditorButton: UIBarButtonItem {
     var index = 0
+    var tapped = false
+    var untappedImage = UIImage()
+    var tappedImage = UIImage()
+    
     weak var delegate: EditorButtonClickDelegate?
     
-    convenience init(image: UIImage, index: Int) {
+    convenience init(image: UIImage, tappedImage: UIImage, index: Int) {
         self.init(image: image.withRenderingMode(.alwaysOriginal), style: .plain, target: nil, action: nil)
         target = self
         action = #selector(clicked)
         self.index = index
         
+        untappedImage = image
+        self.tappedImage = tappedImage
         
     }
     
     
     @objc private func clicked () {
+        if tapped {
+            self.image = untappedImage.withRenderingMode(.alwaysOriginal)
+        } else {
+            self.image = tappedImage.withRenderingMode(.alwaysOriginal)
+        }
+        
+        tapped = !tapped
         delegate?.buttonClickedOnIndex(index: index)
     }
 }
--- WPI_CSA/Helpers/EditorView.swift
+++ WPI_CSA/Helpers/EditorView.swift
@@ -13,7 +13,6 @@ open class EditorView: UIView {
     private var toolbar: UIToolbar
     private var backgroundToolbar: UIToolbar
     
-    private var flag = true
     
     public override init(frame: CGRect) {
         toolbarScroll = UIScrollView()
@@ -56,8 +55,48 @@ open class EditorView: UIView {
         
         
         var buttons = [UIBarButtonItem]()
-        for i in 0 ... 15 {
-            let button = EditorButton(image: #imageLiteral(resourceName: "menuExpanded.png"), index: i)
+        for i in 0 ... 19 {
+            var button: EditorButton!
+            
+            switch (i){
+            case 0:
+                button = EditorButton(image: #imageLiteral(resourceName: "Bold.png"), tappedImage: #imageLiteral(resourceName: "BoldTapped.png"), index: i)//bold
+            case 1:
+                button = EditorButton(image: #imageLiteral(resourceName: "Italic.png"), tappedImage: #imageLiteral(resourceName: "ItalicTapped.png"), index: i)//italic
+            case 2:
+                button = EditorButton(image: #imageLiteral(resourceName: "Underline.png"), tappedImage: #imageLiteral(resourceName: "UnderlineTapped.png"), index: i)//underline
+            case 4:
+                button = EditorButton(image: #imageLiteral(resourceName: "AlignLeft.png"), tappedImage: #imageLiteral(resourceName: "AlignLeftTapped.png"), index: i)//left
+            case 5:
+                button = EditorButton(image: #imageLiteral(resourceName: "AlignCenter.png"), tappedImage: #imageLiteral(resourceName: "AlignCenterTapped.png"), index: i)//center
+            case 6:
+                button = EditorButton(image: #imageLiteral(resourceName: "AlignRight.png"), tappedImage: #imageLiteral(resourceName: "AlignRightTapped.png"), index: i)//right
+            case 8:
+                button = EditorButton(image: #imageLiteral(resourceName: "TextSize15.png"), tappedImage: #imageLiteral(resourceName: "TextSize15Tapped.png"), index: i)//15
+            case 9:
+                button = EditorButton(image: #imageLiteral(resourceName: "TextSize20.png"), tappedImage: #imageLiteral(resourceName: "TextSize20Tapped.png"), index: i)//20
+            case 10:
+                button = EditorButton(image: #imageLiteral(resourceName: "TextSize36.png"), tappedImage: #imageLiteral(resourceName: "TextSize36Tapped.png"), index: i)//36
+            case 11:
+                button = EditorButton(image: #imageLiteral(resourceName: "TextSize72.png"), tappedImage: #imageLiteral(resourceName: "TextSize72Tapped.png"), index: i)//72
+            case 13:
+                button = EditorButton(image: #imageLiteral(resourceName: "ColorBlack.png"), tappedImage: #imageLiteral(resourceName: "ColorBlackTapped.png"), index: i)//black
+            case 14:
+                button = EditorButton(image: #imageLiteral(resourceName: "ColorRed.png"), tappedImage: #imageLiteral(resourceName: "ColorRedTapped.png"), index: i)//red
+            case 15:
+                button = EditorButton(image: #imageLiteral(resourceName: "ColorBlue.png"), tappedImage: #imageLiteral(resourceName: "ColorBlueTapped.png"), index: i)//blue
+            case 16:
+                button = EditorButton(image: #imageLiteral(resourceName: "ColorYellow.png"), tappedImage: #imageLiteral(resourceName: "ColorYellowTapped.png"), index: i)//yellow
+            case 17:
+                button = EditorButton(image: #imageLiteral(resourceName: "ColorGray.png"), tappedImage: #imageLiteral(resourceName: "ColorGrayTapped.png"), index: i)//gray
+            case 18:
+                button = EditorButton(image: #imageLiteral(resourceName: "ColorGreen.png"), tappedImage: #imageLiteral(resourceName: "ColorGreenTapped.png"), index: i)//green
+            case 19:
+                button = EditorButton(image: #imageLiteral(resourceName: "ColorPink.png"), tappedImage: #imageLiteral(resourceName: "ColorPinkTapped.png"), index: i)//pink
+            default:
+                button = EditorButton(image: #imageLiteral(resourceName: "EditorSeparator.png"), tappedImage: #imageLiteral(resourceName: "EditorSeparator.png"), index: i)//separator
+            }
+            
             button.delegate = self
             buttons.append(button)
             
@@ -67,12 +106,12 @@ open class EditorView: UIView {
         
         
         let defaultIconWidth: CGFloat = 30
-        let barButtonItemMargin: CGFloat = 11
+        let barButtonItemMargin: CGFloat = 12
         let width: CGFloat = buttons.reduce(0) {sofar, new in
             if let image = new.image {
                 return sofar + image.size.width + barButtonItemMargin
             } else {
-                return sofar + (defaultIconWidth + barButtonItemMargin)
+                return sofar + defaultIconWidth + barButtonItemMargin
             }
         }
         
@@ -91,16 +130,7 @@ open class EditorView: UIView {
 extension EditorView: EditorButtonClickDelegate {
     func buttonClickedOnIndex(index: Int) {
         print(index)
-        if flag {
-            let button = toolbar.items![index]
-            button.image = #imageLiteral(resourceName: "verified.png").withRenderingMode(.alwaysOriginal)
-            flag = false
-        } else {
-            let button = toolbar.items![index]
-            button.image = #imageLiteral(resourceName: "menuExpanded.png").withRenderingMode(.alwaysOriginal)
-            flag = true
-            
-        }
+        
     }
     
     
